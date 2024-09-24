b7571272bf61d80c5afdf7044658097f6ed7e846
Petros Kouzoupis <peterkuzu@gmail.com>
2024-09-24 11:03:02 +0300

    Added Footer and first iteration of Host layout
    Host layout with nested route plus css styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Vans from './components/Vans/Vans';
 import VanDetail from './components/VanDetail/VanDetail';
+import Dashboard from './components/Host/Dashboard';
+import Income from './components/Host/Income';
+import Reviews from './components/Host/Reviews';
 import Layout from './components/Layout';
+import HostLayout from './components/HostLayout';
 import './App.css';
 
 import "./server"
@@ -18,12 +22,14 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/vans' element={<Vans />} />
           <Route path='/vans/:id' element={<VanDetail />} />
+
+          <Route path='/host' element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='income' element={<Income />} />
+            <Route path='reviews' element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
-      <div className='footer'>
-        <p>&copy; 2024 #VANLIFE</p>
-      </div>
-
     </BrowserRouter>
   );
 }
