1ad55bd80d650727e3ab9d7be5a5c6707b3c6784
Petros Kouzoupis <peterkuzu@gmail.com>
2024-10-07 10:51:23 +0300

    Refactor fetch request in Vans for 404 errors
    It needs to be done for every page that uses the same fetch request

--- src/components/Vans/Vans.js
+++ src/components/Vans/Vans.js
@@ -5,14 +5,23 @@ import { getVans } from "../../api";
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
 
     const typeFilter = searchParams.get("type")
     const filteredVans = typeFilter ? vans.filter(van => van.type === typeFilter) : vans
 
     React.useEffect(() => {
         async function loadVans() {
-            const data = await getVans()
-            setVans(data)
+            setLoading(true)
+            try {
+                const data = await getVans()
+                setVans(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
         }
         loadVans()
     }, [])
@@ -43,6 +52,14 @@ export default function Vans() {
             }
             return prevParams
         })
+    }
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
     }
 
     return (
